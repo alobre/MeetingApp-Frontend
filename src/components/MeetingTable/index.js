aa90c06b9b1f463a87acc286c8b3d22f0beabfd2
alobre <51901804+alobre@users.noreply.github.com>
2023-04-24 17:36:37 +0200

    Working on Table Styling

--- src/components/MeetingTable/index.js
+++ src/components/MeetingTable/index.js
@@ -7,11 +7,13 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography"
 
 // den data Parameter wird erst beim Home-ScreenÜbergeben.
 const MeetingTable = ({ data }) => {
   return (
-    <TableContainer component={Paper}>
+    <div id="tableParent">
+    <TableContainer id="tableContainer" component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -25,8 +27,17 @@ const MeetingTable = ({ data }) => {
               key={row.date}
               //  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
-                {row.date}
+              <TableCell component="th" scope="row" align="center">
+              <div id="DateCell">
+                <Typography variant="h7" component="h3">
+                  {row.date}
+                </Typography>
+                <Typography variant="h7" component="h6">
+                  {row.date}
+                </Typography>
+              </div>
+              
+                
               </TableCell>
               <TableCell>{row.title}</TableCell>
             </TableRow>
@@ -34,6 +45,7 @@ const MeetingTable = ({ data }) => {
         </TableBody>
       </Table>
     </TableContainer>
+    </div>
   );
 };
 export default MeetingTable;
